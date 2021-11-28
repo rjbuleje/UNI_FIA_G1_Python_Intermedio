10c5dfca48355b264629eccbbea462c4f1f3aec8
Robert_Buleje <rbulejedel@uni.pe>
2021-11-27 22:54:06 -0500

    Archivo de ejemplo para la clase

---
+++ ejemplo_complejidad.py
@@ -0,0 +1,16 @@
+def f(x):
+
+    respuesta = 0
+
+    for i in range(1000):
+        respuesta += 1
+
+    for i in range(x):
+        respuesta += x
+
+    for i in range(x):
+        for i in range(x):
+            respuesta += 1
+            respuesta += 1
+
+    return respuesta
