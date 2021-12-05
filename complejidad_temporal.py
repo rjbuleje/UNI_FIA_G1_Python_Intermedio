eab062809fd294f4bf1676d798088aa5ff27473b
Robert_Buleje <rbulejedel@uni.pe>
2021-12-04 23:51:30 -0500

    Cambios2

--- complejidad_temporal.py
+++ complejidad_temporal.py
@@ -11,7 +11,7 @@ def factorial_r(n):
 def factorial_for(n):
     if n == 0 or n == 1:
         return 1
-    acumulador = 1
+    acumulador = 1.0
     for i in range(1,n+1):
         acumulador *= i
     return acumulador
@@ -21,9 +21,9 @@ def factorial_for(n):
 
 if __name__ == "__main__":
     beginning = time.time()
-    factorial_r(100)
+    factorial_r(10000)
     step_1 = time.time()
-    factorial_for(100)
+    factorial_for(10000)
     step_2 = time.time()
     print(step_1 - beginning)
     print(step_2 - step_1)
